08c336aafd3575db3f3420fff7e0a8388bfc0c29
Alejandro <alextorreszgz@gmail.com>
2021-06-09 01:18:09 +0200

    Add userId

--- src/events/KeyUpdatedEventInterface.ts
+++ src/events/KeyUpdatedEventInterface.ts
@@ -3,6 +3,7 @@ import { SubjectEnum } from "./SubjectsEnum";
 export interface KeysUpdatedEventInterface {
   subject: SubjectEnum.KeysUpdated;
   data: {
+    userId: string;
     apiKey: string;
     secretKey: string;
   };
